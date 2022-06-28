380ebaf107838ade2373069f0234fc8c462f7f51
Lucas <lucasrsilva.dev@gmail.com>
2022-06-28 18:50:27 -0300

    cadastrar receita completo

--- src/receitas.cpp
+++ src/receitas.cpp
@@ -19,19 +19,20 @@ void pesquisarReceitaPorNome(MYSQL *conexao, char *query)
     if (mysql_query(conexao, query) != 0)
     {
         system("clear");
-        cout << "Oops.. nenhuma receita encontrada.\n\n";
+        cout << "Oops.. erro inesperado.\n\n";
+        return;
     }
 
-    else 
+    MYSQL_RES *resultado = mysql_store_result(conexao);
+    if(mysql_num_rows(resultado) > 0)
     {
-        mysql_query(conexao, "SELECT * FROM receitas");
-        MYSQL_RES *resultado = mysql_store_result(conexao);
-        // int n_col = mysql_num_fields(resultado);
         MYSQL_ROW linha; // colunas
-        
+        string receita_id;
+
         while ((linha = mysql_fetch_row(resultado)))
         {
-            cout << "| Receita: " << linha[2];
+            receita_id = linha[0];
+            cout << "Receita: " << linha[2];
             cout << " | Rendimento: " << linha[3];
             cout << " | Tempo: " << linha[4];
             cout << " | Info: " << linha[5] << endl << endl;
@@ -39,7 +40,28 @@ void pesquisarReceitaPorNome(MYSQL *conexao, char *query)
 
         // imprimir detalhes da receita
 
+        strcpy(query, ("SELECT * FROM ingredientes where receita_id = '"+ receita_id +"'").c_str());
+        // cout << endl << query << endl;
+        mysql_query(conexao, query);
+        resultado = mysql_store_result(conexao);
+        // int n_col = mysql_num_fields(resultado);
+        
+        cout << "Ingrediente - Quantidade - Unidade";
+        while((linha = mysql_fetch_row(resultado))) 
+        {
+            cout << "\n" << linha[1];
+            cout << " - " << linha[2];
+            cout << " - " << linha[3];
+        }
+
+        cout << endl << endl;
         mysql_free_result(resultado);
+    }
+
+    else 
+    {
+        system("clear");
+        cout << "Oops... receita não existe\n";
     }
 }
 
@@ -65,21 +87,91 @@ void cadastrarReceita(MYSQL *conexao, char *query)
     string info;
     getline(cin, info);
 
-    // get user id sem 0x
-    // (UNHEX(REPLACE('52EF9FA6F1A811EC88721CBFCE373CA6', '-', '')), 'Bolo de Cenoura', 1, 60, '');
-
     cout << "Usuario: ";
-    string usuario;
+    int usuario;
     cin >> usuario;
     cin.clear();
 
-    strcpy(query, ("INSERT INTO receitas (usuario_id, nome, rendimento, tempo, info) VALUES (UNHEX(REPLACE('"+ usuario +"', '-', '')), '"+ receita + "','"+ to_string(rendimento) +"','"+ to_string(tempo) +"','"  + info +"')").c_str());
+    strcpy(query, ("INSERT INTO receitas (usuario_id, nome, rendimento, tempo, info) VALUES ("+ to_string(usuario) + ",'" + receita +  "'," + to_string(rendimento) + "," + to_string(tempo) + ",' "+ info +"')").c_str());
+    mysql_query(conexao, query);
+    
+    // cadastrar ingredientes da receita
 
-    if (mysql_query(conexao, query) != 0)
+    strcpy(query, ("SELECT * FROM receitas WHERE usuario_id = "+ to_string(usuario) + " AND nome = '" + receita + "'").c_str());
+    
+    mysql_query(conexao, query);
+    MYSQL_RES *resultado = mysql_store_result(conexao);
+    // int n_col = mysql_num_fields(resultado);
+    MYSQL_ROW linha; // colunas
+    linha = mysql_fetch_row(resultado);
+    string receita_id = linha[0];
+
+    cout << "Número de ingredientes: ";
+    int num_ingredientes;
+    cin >> num_ingredientes;
+    while(getchar() != '\n');
+    int i=1;
+
+    while(num_ingredientes--) 
     {
-        system("clear");
-        cout << "Oops.. não consegui cadastrar sua receita.\n\n";
+        cout << "\nIngrediente " << i << ": ";
+        string ingrediente;
+        getline(cin, ingrediente);
+        cin.clear();
+
+        cout << "Quantidade: ";
+        float quantidade;
+        cin >> quantidade;
+        while(getchar() != '\n');
+
+        cout << "Unidade: ";
+        string unidade;
+        getline(cin, unidade);
+        cin.clear();
+
+        strcpy(query, ("INSERT INTO ingredientes (receita_id, nome, quantidade, unidade) VALUES ("+ receita_id + ",'" + ingrediente + "'," + to_string(quantidade) +  ",'" + unidade +"')").c_str());
+        mysql_query(conexao, query);
+        i++;
     }
 
-    else system("clear");
+    // cadastrar etapas da receita
+
+    cout << "\nNúmero de etapas: ";
+    int num_etapas;
+    cin >> num_etapas;
+    while(getchar() != '\n');
+    int etapa=1;
+
+    while(num_etapas--) 
+    {
+        cout << "\nTitulo da etapa " << etapa << ": ";
+        string titulo;
+        getline(cin, titulo);
+        cin.clear();
+            
+        strcpy(query, ("INSERT INTO receitas_etapas (receita_id, numero, titulo) VALUES ("+ receita_id + "," + to_string(etapa) + ",'" + titulo +"')").c_str());
+        mysql_query(conexao, query);
+        
+        cout << "\nNúmero de passos da etapa " << etapa << ": ";
+        int num_passos;
+        cin >> num_passos;
+        while(getchar() != '\n');
+
+        for(int sequencia=1; sequencia <= num_passos; sequencia++)
+        {
+            cout << "\nPasso " << sequencia << ": ";
+            string instrucao;
+            getline(cin, instrucao);
+            cin.clear();
+
+            strcpy(query, ("INSERT INTO receitas_passos(receita_id, etapa_numero, sequencia, instrucao) VALUES ("+ receita_id + "," + to_string(etapa) + "," + to_string(sequencia) +  ",'" + instrucao + "')").c_str());
+            mysql_query(conexao, query);
+            cout << endl << query << endl;
+        }
+        etapa++;
+    }
+
+    mysql_free_result(resultado);
+    system("clear");
+    cout << "Receita cadastrada.\n\n";
 }
